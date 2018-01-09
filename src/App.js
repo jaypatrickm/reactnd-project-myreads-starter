a4bdd06c19184de248b548756322c54fd9475367
Jaypatrick <JaypatrickM@gmail.com>
2018-01-08 23:07:15 -0800

    Add BooksAPI, get all books and show in listbooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import AddBook from './AddBook'
 import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
@@ -21,7 +21,8 @@ class BooksApp extends React.Component {
       <div className="app">
 
         <Route exact path='/' render={() => (
-          <ListBooks />
+          <ListBooks
+           books={this.state.books} />
           
         )}/>
           
